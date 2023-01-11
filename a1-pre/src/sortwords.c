b77d63aa981d388b4a1e315728d61d5436762ea7
salomaestro <chris10an.salomonsen@gmail.com>
2023-01-12 00:33:25 +0100

    Seems to be stuck in an infinite loop

--- a1-pre/src/sortwords.c
+++ a1-pre/src/sortwords.c
@@ -1,7 +1,24 @@
-#include "list.h"
 #include "common.h"
+#include "list.h"
+#include "list_util.h"
+
 
 int main(int argc, char **argv)
 {
+    for (int i = 1; i<argc; ++i)
+    {
+        char *filename = argv[i];
+
+        printf("%d\n", i);
+
+        list_t *list = list_create(compare_char);
+
+        printf("start tokenizing %s\n", filename);
+        tokenize_file((FILE *)filename, list);
+        printf("end tokenizing %s\n", filename);
+        
+        printf("begin printing");
+        list_print(list);
+    }
     return 0;
 }
